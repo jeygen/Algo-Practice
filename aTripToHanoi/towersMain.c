1b2fbc935260670e0b991cdb1536251f58e43b25
Surface Bob <joshua.eygenraam@ryerson.ca>
2022-01-25 09:55:59 -0500

    Updated towersMain.c to prevent bad args

--- aTripToHanoi/towersMain.c
+++ aTripToHanoi/towersMain.c
@@ -1,15 +1,48 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "towers.h"
 
 int main(int argc, char **argv)
 {
-    int n = 3;
-    int from = 1;
-    int dest = 2;
-    if (argc > 1) {
-        n = atoi(argv[1]);
-    }
-    towers(n, from, dest);
-    exit(0);
+	int n = 3;
+	int from = 1;
+	int dest = 2;
+	int *p = (int *) malloc(argc * sizeof(int));
+	int i = 0;
+
+	for (i = 0; i < argc; i++) 
+		p[i] = atoi(argv[i]);
+
+	if (argc == 1)
+		towers(n, from, dest);
+	else if (argc == 2) {
+		n = p[1];
+		towers(n, from, dest);
+	}
+	else if (argc == 4 && p[2] != p[3] && p[2] < 4 && p[2] > 0 && p[3] < 4 && p[3] > 0) {
+		n = p[1];
+		from = p[2];
+		dest = p[3];
+		towers(n, from, dest);
+	}
+	else 
+		fprintf(stderr, "\nInput args are not acceptable\n");
+
+	free(p);
+/*
+	if (argc == 1)
+		towers(n, from, dest);
+	else if (argc == 2)
+		n = atoi(argv[1]);
+	else if (argc == 4 && argv[2] != argv[3] && argv[2] < 4 && argv[2] > 0 && argv[3] < 4 && argv[3] > 0) {
+		n = atoi(argv[1]);
+		from = atoi(argv[2]);
+		dest = atoi(argv[3]);
+	}
+	else 
+		fprintf(stderr, "\nInput args are not acceptable\n");
+*/
+	
+	exit(0);
 }
 
