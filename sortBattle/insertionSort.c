1d0b4a4f83c1fc4363ee505a7aaa7225da615ac1
jeygen <jeygenra@gag.ee.ryerson.ca>
2022-02-02 21:51:05 -0500

    The sort methods are ready to be altered to accomodate the metric functions

--- sortBattle/insertionSort.c
+++ sortBattle/insertionSort.c
@@ -1,5 +1,21 @@
 #include "mySort.h"
 
-void mySort(int array[], unsigned int first, unsigned int last)
-    {
+void mySort(int arr[], unsigned int first, unsigned int last) {
+// void insertionSort(int arr[], int n)
+    int i, key, j, n;
+    n = last - first;
+    for (i = 1; i < n; i++) { // notice i starts from 1 instead of 0
+        key = arr[i]; // Assigning a key to 1st unsorted element, elements to the right are "unsorted"
+        j = i - 1; // j is being assigned 1 value below each i value, the "sorted" element
+
+        /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+        while (j >= 0 && arr[j] > key) { // This slides everything greater to the right to "make room" for insertion
+            arr[j + 1] = arr[j]; // 
+            j = j - 1; // j decrements
+        }
+        arr[j + 1] = key; // this is the insertion
     }
+}
+    
--- sortBattle/mergeSort.c
+++ sortBattle/mergeSort.c
@@ -1,5 +1,94 @@
 #include "mySort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-void mySort(int array[], unsigned int first, unsigned int last)
-    {
+void mySort(int arr[], unsigned int l, unsigned int r);
+void merge(int *arr, int l, int mid, int r); 
+
+void mySort(int arr[], unsigned int l, unsigned int r) {
+//void mySort(int *arr, int l, int r) { // first/left index, last/right index // Splits deck into 2 piles
+    if (l < r) {
+        int mid = (l + r) / 2;
+        mySort(arr, l, mid); // All the lefts
+        mySort(arr, mid + 1, r); // All the rights
+        merge(arr, l, mid, r);
     }
+}
+
+void merge(int *arr, int l, int mid, int r) {
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+    int *L = (int*) malloc(n1 * (sizeof(int))); // pseudo says L[1..n+1]
+    int *R = (int*) malloc(n2 * (sizeof(int)));
+    if ( L == NULL || R == NULL)
+        exit(0);
+    int i, j, k;
+    i = j = k = 0;
+
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[mid + 1 + j];
+
+    i = j = 0;
+    k = l;
+
+    /* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	/* Copy the remaining elements of L[], if there
+	are any */
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	/* Copy the remaining elements of R[], if there
+	are any */
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+
+    free(L);
+    free(R);
+        
+}
+
+
+/*
+void merge(int *A, int *B, int sizeA, int sizeB, int *C) { 
+    int i, j, k;
+    i = j = k = 0;
+
+    while (i < sizeA && j < sizeB) {
+            if (A[i] < B[i])
+                C[k++] = A[i++];
+            else    
+                C[k++] = B[j++];
+    }
+
+    for(; i < sizeA; i++) // i keeps prev value as not assigned
+        C[k++] = A[i];
+    for(; j < sizeB; j++)
+        C[k++] = B[j];
+}
+*/
+
+
+
