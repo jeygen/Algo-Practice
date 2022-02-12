64c0a1e97701c97740ab15f071eae47aa1586f28
jeygen <83786292+jeygen@users.noreply.github.com>
2022-02-12 18:26:42 -0500

    Updated Readme

--- sortBattle/runSorts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Insertion Sort with 35 unsorted numbers"
-./insertionSort < numList35.txt
-echo " "
-echo "Merge Sort with 35 unsorted numbers"
-./mergeSort < numList35.txt
-echo " "
-
