15d5e194d17873039691d730f257fbb32be42222
jeygen <83786292+jeygen@users.noreply.github.com>
2022-02-19 23:01:14 -0500

    Update simState.c

--- stateMachine/simState.c
+++ stateMachine/simState.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char * argv[])
-{
+#include "commands.h"
+
+typedef struct state_tag {
+    char name;
+    char zeroState;
+    char oneState;
+    int alive;
+} state_t;
+
+int main(int argc, char * argv[]) {
     exit(0);
 }
 
