a5ac3b517d00d1b7b7c414fd8855ba19b30cee91
jeygen <83786292+jeygen@users.noreply.github.com>
2022-02-20 16:25:47 -0500

    Update simState.c

--- stateMachine/simState.c
+++ stateMachine/simState.c
@@ -10,6 +10,41 @@ typedef struct state_tag {
 } state_t;
 
 int main(int argc, char * argv[]) {
+    state_t A = { .name = 'A',
+                  .zeroState = 'H',
+                  .oneState = 'G',
+                  .alive = 1};
+    state_t B = { .name = 'A',
+                  .zeroState = 'H',
+                  .oneState = 'G',
+                  .alive = 1};
+    state_t C = { .name = 'A',
+                  .zeroState = 'H',
+                  .oneState = 'G',
+                  .alive = 1};
+    state_t D = { .name = 'A',
+                  .zeroState = 'H',
+                  .oneState = 'G',
+                  .alive = 1};
+    state_t E = { .name = 'A',
+                  .zeroState = 'H',
+                  .oneState = 'G',
+                  .alive = 1};
+    state_t F = { .name = 'A',
+                  .zeroState = 'H',
+                  .oneState = 'G',
+                  .alive = 1};
+    state_t G = { .name = 'A',
+                  .zeroState = 'H',
+                  .oneState = 'G',
+                  .alive = 1};
+    state_t G = { .name = 'A',
+                  .zeroState = 'H',
+                  .oneState = 'G',
+                  .alive = 1};
+    
+   
+    
     exit(0);
 }
 
