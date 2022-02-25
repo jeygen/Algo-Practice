ef706d41eb72858204454039d9eed3bed975d19b
jeygen <83786292+jeygen@users.noreply.github.com>
2022-02-24 19:13:41 -0500

    Updated simState.c

--- stateMachine/simState.c
+++ stateMachine/simState.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "commands.h"
+
+#define TOTAL 3
 
 
 
@@ -14,10 +17,12 @@ typedef struct state_tag {
 
 void noCommands(state_t *);
 void oneZeroCommands(int i, state_t *);
+void print(state_t **arr, int size);
 
 // Gobal var
-//extern struct state_tag currentState;
+struct state_tag *cS;
 //extern struct state_tag *A, *B, *C, *D, *E, *F, *G, *H;
+//char cS;
 
 // Driver
 int main(int argc, char * argv[]) {
@@ -125,42 +130,86 @@ int main(int argc, char * argv[]) {
     state_t *Hp = &H;
     */
 
-    currentState = A;
-    
-    noCommands(currentState);
+    currentState = A; // currently not needed
+    cS = A;
 
-    // Check if cmd args are legal
+    // Making 1D array of pointers to print TODO figure out how to pass to function
+    state_t** arr = (state_t**)malloc(TOTAL * sizeof(state_t*));
+    int i = 0;
+    for (i = 0; i < TOTAL; i++) {
+        arr[i] = (state_t*)malloc(TOTAL * sizeof(state_t));
+    }
+    *arr = A;
+    *(arr+1) = B;
+    *(arr+2) = C;
+    fprintf(stdout, "Name: %c, NextOne: %c, NextZero: %c\n", arr[0]->name, arr[0]->oneState->name, arr[0]->zeroState->name);
+    //noCommands(cS);
+
+    // to do Check if cmd args are legal
+    if (argc > 3) {
+        fprintf(stderr, "Incorrect number of args\n");
+    }
+    /*
+    if ((argc == 1 && (argv[1] != "0" || argv[1] != "1" || argv[1] != 'p' || argv[1] != 'g' || argv[1] != 'd'))
+    || (argc == 2 && (argv[1] != 'd' || argv[2] != "0" || argv[2] != "1"))
+    || (argc == 3 && (argv[1] != 'c' || argv[2] != "0" || argv[2] != "1" || argv[3] != "A" || argv[3] != "B"
+    || argv[3] != "C" || argv[3] != "D" || argv[3] != "E" || argv[3] != "F" || argv[3] != "G" || argv[3] != "H"))) {
+        fprintf(stderr, "Incorrect args. Input: '0', '1', 'c <0 or 1> <A-H>', 'p', 'g', 'd' or 'd <A-H>'\n");
+    }
+    */
+
+
+    //print(*arr, TOTAL);       
+    
+    
+    // One/Zero Commands
     if (argc == 2) {
             if (atoi(argv[1]) == 1) 
-                oneZeroCommands(1, currentState);
-            if (atoi(argv[1]) == 0)
-                oneZeroCommands(0, currentState);
+                oneZeroCommands(1, cS); 
+            else if (atoi(argv[1]) == 0) 
+                oneZeroCommands(0, cS);
+            else if (atoi(argv[1]) == 112)
+                print(arr, TOTAL);
             else {
-                fprintf(stderr, "Command must be \"1\" or \"0\"");
+                fprintf(stderr, "Single Command must be \"1\" or \"0\" or \"p\"\n");
                 exit(0);
             }
     }
-    noCommands(currentState);
+    noCommands(cS);
     
+    // Change Command
+    //if (argc == 3){} 
+    //free(cS);
+    //free(A);
+    //free(B);
+    //free(C);
     exit(0);
 }
 
-// Greets user with current state
-void noCommands(state_t *currentState) {
+// EFFECTS: stdout greets user with current state
+void noCommands(state_t *currentState) { 
     fprintf(stdout, "The current state is: %c\n", currentState->name);
 }
 
-
+// MODIFIES: currentState  
+// EFFECTS: stdout currentState's nextState and then change current state to next state
 void oneZeroCommands(int i, state_t *currentState) {
     if (i == 1) {
         fprintf(stdout, "The next state of \"%c\" is \"%c\"\n", currentState->name, currentState->oneState->name);
         //currentState = currentState->oneState;
+        cS = currentState->oneState;
     }        
-    if (i == 0) {
+    else if (i == 0) {
         fprintf(stdout, "The next state of \"%c\" is \"%c\"\n", currentState->name, currentState->zeroState->name);
-        //currentState = currentState->zeroState;
+        cS = currentState->zeroState;
     }
     else
         fprintf(stderr, "Incorrect input");
 }
 
+void print(state_t **arr, int size) {
+    int i = 0;
+    for(i = 0; i < size; i++) {
+        //fprintf(stdout, "Name: %c, NextOne: %c, NextZero: %c\n", *(arr+i)->name, *(arr+i)->oneState->name, *(r+i)->zeroState->name);
+    }
+}
