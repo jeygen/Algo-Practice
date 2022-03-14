b2b8baa17e3c5daf3f3c5dca1a06747589d38ba4
jeygen <jeygenra@bele.ee.ryerson.ca>
2022-03-13 20:23:55 -0400

    Part 2 is functioning correctly

--- xmlValidation/part2Main.c
+++ xmlValidation/part2Main.c
@@ -8,6 +8,8 @@ extern int isEmpty(); // I added this, check if thats allowed
 /*
 * Part 2
 * Same as part 1, but now counting and saving each tag
+* Added a direct list/array that has it's 1st element as a key and 2nd element as a counter
+* Should ask if there is a better way to do this, like maybe using a linked list?
 */
 
 int main(int argc, char * argv[])
@@ -18,9 +20,9 @@ int main(int argc, char * argv[])
 	int endTag; // for char after '</'
 	int endTagcloser;
 	//int **table;
-	int j = 0;
-	int i = 'a';
-	int table [26][2] = {    {'a', 0}, {'b', 0}, {'c', 0}, {'d', 0}, {'e', 0}, 
+	//int j = 0;
+	int i = 0;
+	char table [26][2] = {    {'a', 0}, {'b', 0}, {'c', 0}, {'d', 0}, {'e', 0}, 
 				 {'f', 0}, {'g', 0}, {'h', 0}, {'i', 0}, {'j', 0},
 				 {'k', 0}, {'l', 0}, {'m', 0}, {'n', 0}, {'o', 0},
 				 {'p', 0}, {'q', 0}, {'r', 0}, {'s', 0}, {'t', 0},
@@ -28,9 +30,11 @@ int main(int argc, char * argv[])
 	  		         {'z', 0}}; 
 
 
-	for (i = 'a'; i <= 'z'; i++) {
+	/*
+	for (i = 0; i < 26; i++) {
 		fprintf(stdout, "%c %d\n", table[i][0], table[i][1]);
-	} // this array is printing wonky
+	} 
+	*/
 		
 
 	while ((ch = getchar()) != EOF) {
@@ -51,8 +55,8 @@ int main(int argc, char * argv[])
 
 				if (endTagcloser == '>') {
 					if (pop() == endTag) {
-						for (i = 'a'; i <= 'z'; i++) {
-							if (table[i][1] > 0)
+						for (i = 0; i < 26; i++) {
+							if ((int)table[i][1] > 0)
 								fprintf(stdout, "%c %d\n", table[i][0], table[i][1]);
 						}	
 						fprintf(stdout, "Valid\n");
@@ -79,7 +83,10 @@ int main(int argc, char * argv[])
 
 			if (islower(startTag) && startTagcloser == '>') {
 				push(startTag);
-				table[startTag][j++];
+				for (i = 0; i < 26; i++) {
+					if (table[i][0] == startTag)
+						table[i][1]++;
+				}
 			}
 			else { 
 				continue;
